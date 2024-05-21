2e3ab2082e038ae7a3a76ec4e7308e33f6dc39c4
LAPTOP-F25A12VH\Dear <natthaphong.ea@gmail.com>
2024-05-21 13:27:18 +0700

    feat(js): add answer ex1-8 using Built-in Array Functions

--- ex-6.js
+++ ex-6.js
@@ -1,12 +1,20 @@
-const carCollection = ["toyota", "fiat", "honda", "bmw"];
+const carCollection = ['toyota', 'fiat', 'honda', 'bmw'];
 
 function updateCarCollection(carBrand) {
   // Start coding here
+  if (carCollection.includes(carBrand)) {
+    return `${carBrand} already exists in position ${
+      carCollection.indexOf(carBrand) + 1
+    } of the car collection.`;
+  }
+  carCollection.push(carBrand);
+  return `New car collection is: ${carCollection}`;
 }
 
 //ผลลัพธ์ที่ควรได้จาก Example case
 //ในกรณีที่ยังไม่มียี่ห้อรถใน carCollection
-console.log(updateCarCollection("audi")); //new car collection is : toyota,fiat,honda,bmw,audi.
+console.log(updateCarCollection('audi')); //new car collection is : toyota,fiat,honda,bmw,audi.
 
 //ในกรณีที่มียี่ห้อรถใน carCollection
-console.log(updateCarCollection("toyota")); //toyota has already existed in the 1 position of car collection.
+console.log(updateCarCollection('toyota')); //toyota has already existed in the 1 position of car collection.
+// toyota already exists in position 1 of the car collection.
